daa127857a009de5c14be4e6b0766743420ebd9a
James Lenahan <james.k.lenahan@gmail.com>
2020-04-13 20:55:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ function moveDodgerLeft() {
     
   }
 }
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+  if (left > 0) {
+    dodger.style.left = `${left +1}px`;
+    
+  }
+}
